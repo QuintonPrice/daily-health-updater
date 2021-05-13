2c4555893c0429c0ba7af6d8a6e3adffcbf05ffc
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-05-12 17:02:49 -0700

    initial commit

---
+++ aswwu3.py
@@ -0,0 +1,21 @@
+import time
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
+
+
+def main():
+  names = [ "Benko", "DePaula", "Fairchild", "Hartman", "Herbel", 
+           "Hernandez", "Jones", "Moody", "Price", "Reklai", "Riggs", 
+           "Smith", "Sukachevin", "Taylor", "Thomsen", "Tinker" ]
+  start = time.time()
+  for name in names:
+    r = requests.get("https://aswwu.com/server/search/1920/" + name)
+    print(name.encode() in r.content, r.status_code)
+  end = time.time()
+  print(end - start)
+
+
+if __name__ == "__main__":
+    main()
